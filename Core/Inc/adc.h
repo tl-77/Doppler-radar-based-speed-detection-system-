e3552a293adf19baaee2bc26337dc034e7daff99
tl-77 <127114770+tl-77@users.noreply.github.com>
2024-12-17 23:16:21 +0800

    Add files via upload

---
+++ Core/Inc/adc.h
@@ -0,0 +1,15 @@
+/*
+ * adc.h
+ *
+ *  Created on: Dec 13, 2024
+ *      Author: yitin
+ */
+
+#ifndef SRC_ADC_H_
+#define SRC_ADC_H_
+#include "main.h"
+
+void ADCmethod(ADC_HandleTypeDef *hadc1,ADC_HandleTypeDef *hadc2,UART_HandleTypeDef *huart2);
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc1) ;
+
+#endif /* SRC_ADC_H_ */
---
+++ Core/Inc/comparator.h
@@ -0,0 +1,18 @@
+/*
+ * comparator.h
+ *
+ *  Created on: Dec 13, 2024
+ *      Author: yitin
+ */
+
+#ifndef SRC_COMPARATOR_H_
+#define SRC_COMPARATOR_H_
+
+#include "main.h"
+
+void compMethod(COMP_HandleTypeDef *hcomp1,TIM_HandleTypeDef *htim16,ADC_HandleTypeDef *hadc2,UART_HandleTypeDef *huart2);
+void HAL_COMP_TriggerCallback(COMP_HandleTypeDef *hcomp);
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim16);
+
+
+#endif /* SRC_COMPARATOR_H_ */
