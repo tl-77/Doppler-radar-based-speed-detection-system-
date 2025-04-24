b95fff998c3ad791d56e2efb8f818e9eeca222aa
tl-77 <127114770+tl-77@users.noreply.github.com>
2025-04-24 23:46:15 +0800

    Update lcd.h

--- Core/Inc/lcd.h
+++ Core/Inc/lcd.h
@@ -12,7 +12,14 @@
 
 void LCD_Init();
 int processButton(ADC_HandleTypeDef *hadc2);
-void displayLCD(float speed,float freq);
+void displayLCD(float speed,float freq,UART_HandleTypeDef *huart2,UART_HandleTypeDef *huart3);
+void RS485_EnableTransmit(void) ;
+void RS485_EnableReceive(void);
+uint8_t ConvertSpeedToBCD(uint8_t speed) ;
+void ConvertToBinary(uint8_t num, char* binaryStr);
+void RS485_SendSpeed(uint8_t speed,UART_HandleTypeDef *huart2,UART_HandleTypeDef *huart3);
+void display7seg(float speed,UART_HandleTypeDef *huart2,UART_HandleTypeDef *huart3);
+
 float frequencyToSpeed (float frequency);
 float msToKmh (float speed);
 float msToMph(float speed);
