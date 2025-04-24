24b711ecbf4f1901771b761f74d26be8274ebc16
tl-77 <127114770+tl-77@users.noreply.github.com>
2025-04-24 23:45:55 +0800

    Update comparator.h

--- Core/Inc/comparator.h
+++ Core/Inc/comparator.h
@@ -10,7 +10,7 @@
 
 #include "main.h"
 
-void compMethod(COMP_HandleTypeDef *hcomp1,TIM_HandleTypeDef *htim16,ADC_HandleTypeDef *hadc2,UART_HandleTypeDef *huart2);
+void compMethod(COMP_HandleTypeDef *hcomp1,TIM_HandleTypeDef *htim16,ADC_HandleTypeDef *hadc2,UART_HandleTypeDef *huart2, DAC_HandleTypeDef *hdac1,UART_HandleTypeDef *huart3);
 void HAL_COMP_TriggerCallback(COMP_HandleTypeDef *hcomp);
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim16);
 
