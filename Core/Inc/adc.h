deff7914ac63d7507d967707af65168d948a3855
tl-77 <127114770+tl-77@users.noreply.github.com>
2025-04-24 23:45:38 +0800

    Update adc.h

--- Core/Inc/adc.h
+++ Core/Inc/adc.h
@@ -9,7 +9,7 @@
 #define SRC_ADC_H_
 #include "main.h"
 
-void ADCmethod(ADC_HandleTypeDef *hadc1,ADC_HandleTypeDef *hadc2,UART_HandleTypeDef *huart2);
+void ADCmethod(ADC_HandleTypeDef *hadc1,ADC_HandleTypeDef *hadc2,UART_HandleTypeDef *huart2,UART_HandleTypeDef *huart3);
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc1) ;
 
 #endif /* SRC_ADC_H_ */
